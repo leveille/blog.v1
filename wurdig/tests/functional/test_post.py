de17b927f62ea71d11b5971fd6963084b5cb54c6
Jason Leveille <leveillej@gmail.com>
2009-04-03 16:41:42 -0400

    Added post tests

--- wurdig/tests/functional/test_post.py
+++ wurdig/tests/functional/test_post.py
@@ -2,6 +2,42 @@ from wurdig.tests import *
 
 class TestPostController(TestController):
 
-    def test_index(self):
-        response = self.app.get(url(controller='post', action='index'))
-        # Test response...
+    def test_archives_404_missing_year(self):
+        response = self.app.get(url(controller='post', action='archive'), status=404)
+        
+    def test_archives_year_only(self):
+        response = self.app.get(url(controller='post', action='archive', year='2008'))
+        assert 'Fifth test post' in response
+        assert '<p>This is the fifth test post</p>' in response
+        assert 'REQUEST_METHOD' in response.req.environ
+        
+    def test_archives_year_and_month(self):
+        response = self.app.get(url(controller='post', 
+                                    action='archive', 
+                                    year='2009', month='03'))
+        assert 'Second test post' in response
+        assert '<p>This is the second test post</p>' in response
+        assert 'REQUEST_METHOD' in response.req.environ
+        
+    def test_view(self):
+        response = self.app.get(url(controller='post', 
+                                    action='view', 
+                                    year='2009', 
+                                    month='03', slug='third-test-post'))
+        # assert hasattr(response, 'cache') is True
+        assert 'Third test post' in response
+        assert '<p>This is the third test post</p>' in response
+        assert 'REQUEST_METHOD' in response.req.environ
+        
+    def test_view_404_missing_slug(self):
+        response = self.app.get(url(controller='post', 
+                                    action='view', 
+                                    year='2009', 
+                                    month='03'), status=404)
+        
+    def test_view_404_invalid_slug(self):
+        response = self.app.get(url(controller='post', 
+                                    action='view', 
+                                    year='2009', 
+                                    month='03', 
+                                    slug='foobar'), status=404)
--- wurdig/websetup.py
+++ wurdig/websetup.py
@@ -58,6 +58,17 @@ def setup_app(command, conf, vars):
     post1.title = u'First test post'
     post1.slug = u'first-test-post'
     post1.content = u'<p>This is the first test post</p>'
+    post1.created_on = datetime.datetime(2008, 3, 17, 12, 30, 45)
+    meta.Session.add(post1)
+    meta.Session.flush()
+    
+    log.info("Adding fifth post...")
+    post1 = model.Post()
+    post1.title = u'Fifth test post'
+    post1.slug = u'fifth-test-post'
+    post1.content = u'<p>This is the fifth test post</p>'
+    post1.created_on = datetime.datetime(2008, 3, 17, 12, 30, 45)
+    post1.posted_on = datetime.datetime(2008, 3, 18, 12, 30, 45)
     meta.Session.add(post1)
     meta.Session.flush()
     
@@ -66,8 +77,29 @@ def setup_app(command, conf, vars):
     post2.title = u'Second test post'
     post2.slug = u'second-test-post'
     post2.content = u'<p>This is the second test post</p>'
-    post2.posted_on = datetime.datetime.now()
+    post2.created_on = datetime.datetime(2009, 3, 24, 12, 30, 45)
+    post2.posted_on = datetime.datetime(2009, 3, 24, 12, 30, 45)
     meta.Session.add(post2)
+    meta.Session.flush()
+    
+    log.info("Adding third post...")
+    post3 = model.Post()
+    post3.title = u'Third test post'
+    post3.slug = u'third-test-post'
+    post3.content = u'<p>This is the third test post</p>'
+    post3.created_on = datetime.datetime(2009, 3, 25, 12, 30, 45)
+    post3.posted_on = datetime.datetime(2009, 3, 25, 12, 30, 45)
+    meta.Session.add(post3)
+    meta.Session.flush()
+    
+    log.info("Adding fourth post...")
+    post4 = model.Post()
+    post4.title = u'Fourth test post'
+    post4.slug = u'fourth-test-post'
+    post4.content = u'<p>This is the fourth test post</p>'
+    post4.created_on = datetime.datetime(2009, 4, 25, 12, 30, 45)
+    post4.posted_on = datetime.datetime(2009, 4, 25, 12, 30, 45)
+    meta.Session.add(post4)
     meta.Session.flush()
     
     log.info("Adding second post comment...")
