ac7658c4ec53475792eefe45f7d21b634f5b15e7
Jason Leveille <leveillej@gmail.com>
2009-04-12 20:51:09 -0400

    Removed has_delete_role check

--- wurdig/lib/auth.py
+++ wurdig/lib/auth.py
@@ -5,7 +5,6 @@ from pylons.templating import render_mako as render
 from authkit.authorize.pylons_adaptors import authorize
 
 is_valid_user = ValidAuthKitUser()
-has_delete_role = HasAuthKitRole(['delete'])
 
 def render_signin():
     return render('/derived/account/signin.html').encode('utf-8')
