0b42657f438a1d0b8db1611a498b02d34d501ec1
Jason Leveille <leveillej@gmail.com>
2009-04-14 10:46:47 -0400

    Adding comment support

--- wurdig/lib/helpers.py
+++ wurdig/lib/helpers.py
@@ -4,5 +4,7 @@ Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to templates as 'h'.
 """
 from webhelpers.html.tags import *
+from webhelpers.html import literal
+from webhelpers.text import truncate, chop_at, plural
 from routes import url_for
 from wurdig.lib import auth
