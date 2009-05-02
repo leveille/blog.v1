ea6fd57d6d28d026c45be9385e16a893334960f0
Jason Leveille <leveillej@gmail.com>
2009-05-02 18:01:43 -0400

    Updated display of flickr/delicious/twitter feeds

--- wurdig/lib/helpers.py
+++ wurdig/lib/helpers.py
@@ -6,6 +6,7 @@ available to Controllers. This module is available to templates as 'h'.
 from webhelpers.html.tags import *
 from webhelpers.html.secure_form import secure_form
 from webhelpers.html import literal
+from webhelpers.html.tools import auto_link
 from webhelpers.text import truncate, chop_at, plural
 from routes import url_for
 from wurdig.lib import auth
