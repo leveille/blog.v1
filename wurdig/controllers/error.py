de2d19c8b745aa34a6330be1db5432ec96582d47
Jason Leveille <leveillej@gmail.com>
2009-05-12 22:56:13 -0400

    Added error document support

--- wurdig/controllers/error.py
+++ wurdig/controllers/error.py
@@ -2,10 +2,11 @@ import cgi
 
 from paste.urlparser import PkgResourcesParser
 from pylons import request
+from pylons import tmpl_context as c
 from pylons.controllers.util import forward
 from pylons.middleware import error_document_template
 from webhelpers.html.builder import literal
-from wurdig.lib.base import BaseController
+from wurdig.lib.base import BaseController, render
 
 class ErrorController(BaseController):
 
@@ -22,12 +23,16 @@ class ErrorController(BaseController):
     def document(self):
         """Render the error document"""
         resp = request.environ.get('pylons.original_response')
-        content = literal(resp.body) or cgi.escape(request.GET.get('message', ''))
-        page = error_document_template % \
-            dict(prefix=request.environ.get('SCRIPT_NAME', ''),
-                 code=cgi.escape(request.GET.get('code', str(resp.status_int))),
-                 message=content)
-        return page
+        code = cgi.escape(request.GET.get('code', ''))
+        content = cgi.escape(request.GET.get('message', ''))
+        if resp:
+            content = literal(resp.status)
+            code = code or cgi.escape(str(resp.status_int))
+        if not code:
+            raise Exception('No status code was found')
+        c.code = code
+        c.message = content
+        return render('/derived/error/document.html')
 
     def img(self, id):
         """Serve Pylons' stock images"""
