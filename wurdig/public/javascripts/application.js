20098400be7cd4475a7a7af9e1cb8cb621c66b18
Jason Leveille <leveillej@gmail.com>
2009-05-19 14:55:03 -0400

    Updated comment styleguide display

--- wurdig/public/javascripts/application.js
+++ wurdig/public/javascripts/application.js
@@ -1,26 +1,39 @@
 WURDIG = {};
 WURDIG.app = function(){
+    function fetch_styleguide($container)
+    {
+        if (!WURDIG.app.styleguide_fetched) {
+            $container.load("/comment-styleguide div.wurdig-entry", function(){
+                WURDIG.app.styleguide_fetched = true;
+            });
+        }
+    }
+    function styleguide () 
+    {
+        $guide_href = jQuery('#comment-styleguide');
+        var $guide_container = $guide_href.parent().find('div');
+        fetch_styleguide($guide_container);
+        
+        $guide_href.bind('click', function() {
+            $guide_container.slideToggle('slow');
+            return false;
+        });
+    }
     return {
         styleguide_fetched: false,
         init: function(){
+            styleguide();
             jQuery('#yui-main h2:first').addClass('wurdig-first');
-            jQuery('#comment-styleguide').bind('click', function(){
-                var $guide_container = jQuery(this).parent().find('div');
-                if (!WURDIG.app.styleguide_fetched) {
-                    $guide_container.load("/comment-styleguide div.entry", function(){
-                        WURDIG.app.styleguide_fetched = true;
-                    });
-                }
-                $guide_container.toggleClass('hide');
-                return false;
-            });
             jQuery('div.required label').append(' <span class="error-message">*</span>');
             jQuery("div[id^='comment-'] h4").next('blockquote').addClass('bq-parent');
             jQuery("a[rel='external'], a._blank").bind('click', function(){
                 window.open(this.href);
                 return false;
             });
-            jQuery('#wurdig-header-img h1, #wurdig-header-img h2, #wurdig-header-img h3, #wurdig-header-img p').fadeTo("slow", 0.5);
+            jQuery('#wurdig-header-img h1, \
+                #wurdig-header-img h2, \
+                #wurdig-header-img h3, \
+                #wurdig-header-img p').fadeTo("slow", 0.5);
         }
     };
 }();
