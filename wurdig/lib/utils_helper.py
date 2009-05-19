cdef07613e6668ab898d468abdffeb251edfe973
Jason Leveille <leveillej@gmail.com>
2009-05-18 21:56:31 -0400

    Updated path to header dir for random header
    Minor style modifications

--- wurdig/lib/utils_helper.py
+++ wurdig/lib/utils_helper.py
@@ -5,8 +5,9 @@ __all__ = ['random_header']
 def random_header():
     image = 'header2.jpg'
     try:
-        path = os.getcwd() + os.sep + 'wurdig' + os.sep + 'public' \
-                + os.sep + 'images' + os.sep + 'headers' + os.sep
+        root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        path = root + os.sep + 'public' + os.sep + 'images' \
+                 + os.sep + 'headers' + os.sep
         images = glob.glob(path + '*.jpg')
         image = images.pop(random.randrange(len(images)))
         image = os.path.basename(image)        
