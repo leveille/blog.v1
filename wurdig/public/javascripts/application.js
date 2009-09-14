4f83f86b278946bc467ebb270aa9bd77fa93e8f0
leveille <leveillej@gmail.com>
2009-09-14 15:03:47 -0400

    Added some minor style enhancements

--- wurdig/public/javascripts/application.js
+++ wurdig/public/javascripts/application.js
@@ -7,7 +7,6 @@ WURDIG.app = function(){
             jQuery('#yui-main h2:first').addClass('wurdig-first');
             jQuery('div.required label').append(' <span class="error-message">*</span>');
             jQuery("div[id^='comment-'] h4").next('blockquote').addClass('bq-parent');
-            jQuery('#wurdig-header-img h1, #wurdig-header-img h2, #wurdig-header-img h3, #wurdig-header-img p').fadeTo("slow", 0.5);
         }
     };
 }();
