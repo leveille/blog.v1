36fa656699cc3c9344b36a76ebb2eb7926bdefc0
Jason R. Leveille <leveille@leveille-laptop.(none)>
2009-10-05 22:24:31 -0400

    Updated javascript to pull in about me paragraph

--- wurdig/public/javascripts/application.js
+++ wurdig/public/javascripts/application.js
@@ -1,5 +1,5 @@
-if(!this.WURDIG) {
-    WURDIG = {};   
+if (!this.WURDIG) {
+    WURDIG = {};
 }
 WURDIG.app = function(){
     return {
@@ -7,11 +7,13 @@ WURDIG.app = function(){
             jQuery('#yui-main h2:first').addClass('wurdig-first');
             jQuery('div.required label').append(' <span class="error-message">*</span>');
             jQuery("div[id^='comment-'] h4").next('blockquote').addClass('bq-parent');
-			
-			if (jQuery('body.about').length < 1) {
-				//inject sidebar about me content from about page
-				jQuery('#sidebar-about').load('/about #about-intro');
-			}
+            
+            if (jQuery('body.about').length < 1) {
+                //inject sidebar about me content from about page
+                jQuery('#sidebar-about').load('/about #about-intro', function(){
+                    jQuery('span.hide').removeClass('hide');
+                });
+            }
         }
     };
 }();
